05c87921f5acd0856cfa872180132016260ff3f3
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-03-07 14:35:14 +0600

    Added components "Layout.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import Layout from "./components/Layout/Layout";
+import {Route, Routes} from "react-router-dom";
+import ClientArea from "./container/ClientArea/ClientArea";
+import Admin from "./container/Admin/Admin";
 
 function App() {
 
   return (
-    <>
-    </>
+    <Layout>
+      <Routes>
+        <Route path={"/"} element={(<ClientArea />)} />
+        <Route path={"/admin"} element={(<Admin />)} />
+      </Routes>
+    </Layout>
   );
 }
 
